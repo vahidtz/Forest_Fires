34e0f2586387005f70c964128ffdbf7fc6478ecf
Vahid Mohammadnia <v.mohammadnia83@gmail.com>
2018-11-02 17:42:08 -0500

    project description file was added

--- Code.py
+++ Code.py
@@ -119,3 +119,28 @@ plt.show()
 # As you see even with polynomical the r2 score is a negative value. This shoews that the model acts poorly. We should try more complicated models.
 
 
+
+from string import ascii_letters
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Compute the correlation matrix
+corr =all_features.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
